7e5f2dd467c3b4e9ee4d88246d8bff35e9159ef7
dejan-collin-vinci <dejan.collin@student.vinci.be>
2026-01-16 16:26:10 +0100

    reject now email with space(s)

--- src/emailValide.ts
+++ src/emailValide.ts
@@ -2,5 +2,9 @@ export function isValidEmail(email:String): boolean {
     if(!email.includes('@')){
         return false;
     }
+
+    if(email.includes(' ')){
+        return false;
+    }
     return true;
 }
