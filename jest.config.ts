bb271ffb7e6c6cf1cba4206ce8c8a2513bbb6699
dejan-collin-vinci <dejan.collin@student.vinci.be>
2026-01-16 16:45:43 +0100

    try to fix the action error issues

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,22 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  testMatch: ["**/*.test.ts"],
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'], 
+  
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  
+  reporters: [
+    'default',
+    'jest-junit',
+  ],
+  
 };
+
+export default config;
