7a126d522ecd89408cd9b7cb3ad72ab7e25d2dca
dejan-collin-vinci <dejan.collin@student.vinci.be>
2026-01-16 16:25:15 +0100

    test: check for spaces

--- tests/emailValide.test.ts
+++ tests/emailValide.test.ts
@@ -8,4 +8,10 @@ describe('Email Validation', () => {
     test('should return true for a simple valid email', () => {
         expect(isValidEmail("toto@vinci.be")).toBe(true);
     });
+
+    test('should return false if email contains spaces', () => {
+        expect(isValidEmail("toto @vinci.be")).toBe(false);
+        expect(isValidEmail("toto@ vinci.be")).toBe(false);
+    });
+    
 });
