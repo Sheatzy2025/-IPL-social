652073899502de348ff481a1e783f71dbccd321f
dejan-collin-vinci <dejan.collin@student.vinci.be>
2026-01-16 16:32:20 +0100

    function now return false if no text before or after @

--- src/emailValide.ts
+++ src/emailValide.ts
@@ -6,5 +6,13 @@ export function isValidEmail(email:String): boolean {
     if(email.includes(' ')){
         return false;
     }
+
+    const partialEmail = email.split('@')
+    if(partialEmail.length !== 2){
+        return false;
+    }
+    if (partialEmail[0]?.length === 0 || partialEmail[1]?.length === 0){
+        return false;
+    }
     return true;
 }
